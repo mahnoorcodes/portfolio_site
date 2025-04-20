579734576d697bde3729c788c299726fb773831a
Your Name <you@example.com>
2025-04-20 21:15:27 +0400

    Navbar Complete

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,11 +5,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return(
         <nav className={styles.navbar}>
-            <a href="/" className={styles.navbarLink}>MAHNOOR FAISAL</a>
-            <FontAwesomeIcon icon={faBars} className={styles.menuIcon} alt="menu" />
-            <div className={styles.navbarLinks}>
+            <div className={styles.logoContainer}>
+                <a href="/" className={styles.navbarLink}>MAHNOOR FAISAL</a>
+                <FontAwesomeIcon icon={faBars} className={styles.menuIcon} onClick= {toggleMenu} alt="menu" />
+            </div>
+
+            <div className={`${styles.navbarLinks} ${menuOpen ? styles.show : ''}`}>
                 <a href="/about" >ABOUT</a>
                 <a href="/projects">PROJECTS</a>
                 <a href="/skills">SKILLS</a>
