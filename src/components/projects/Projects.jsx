8c742093af422faac00f1658be2fb56c1fdee68a
Your Name <you@example.com>
2025-04-23 23:05:52 +0400

    Projects Complete

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import styles from './projects.module.css'
+import projects from "../data/projects.json";
+import { ProjectsCard } from './ProjectsCard';
 
 export const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <section className={styles.projectSection} id="projects">
+      <h1 className={styles.title}>PROJECTS</h1>
+      <div className={styles.projects}>
+      {projects.map((projects, id) => {
+        return <ProjectsCard key={id} {...projects} />;
+      })}
+      </div>
+    </section>
+  );
+};
---
+++ src/components/projects/ProjectsCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './projects.module.css'
+
+export const ProjectsCard = ({
+    title, imageSrc, description, skills, githubLink, liveDemo
+}) => {
+    return (
+        <div className={styles.projectCard}>
+            <img src={imageSrc} alt={title} className={styles.image} />
+            <div className={styles.cardContent}>
+                <h3 className={styles.title}>{title}</h3>
+                <p className={styles.description}>{description}</p>
+                {/* <span >Skills:</span> */}
+                    <ul className={styles.skills}>
+                        {skills.map((skill, id) => (
+                            <li key={id} className={styles.skill}>{skill}</li>
+                        ))}
+                    </ul>
+                <div className={styles.links}>
+                    <a href={githubLink} className={styles.link} target="_blank" rel="noopener noreferrer">GitHub Link</a>
+                    <a href={liveDemo} className={styles.link} target="_blank" rel="noopener noreferrer">Live Link</a>
+                </div>
+            </div>
+        </div>
+    );
+};
