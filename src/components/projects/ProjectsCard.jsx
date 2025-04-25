2c13402b1d1e99bdac8b5a7e62f634f6a6872d80
Your Name <you@example.com>
2025-04-25 20:05:26 +0400

    Image Update

--- src/components/projects/ProjectsCard.jsx
+++ src/components/projects/ProjectsCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './projects.module.css'
+
 export const ProjectsCard = ({
     title, imageSrc, description, skills, githubLink, liveDemo
 }) => {
     return (
         <div className={styles.projectCard}>
-            <img src={`/${imageSrc}`} alt={title} className={styles.image} />
+            <img src={imageSrc} alt={title} className={styles.image} />
             <div className={styles.cardContent}>
                 <h3 className={styles.title}>{title}</h3>
                 <p className={styles.description}>{description}</p>
