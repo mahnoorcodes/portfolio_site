715ea5d54ba975ed825c9a419adf1c993e8706c4
Your Name <you@example.com>
2025-04-25 19:56:24 +0400

    ProjImg in Public

--- src/components/projects/ProjectsCard.jsx
+++ src/components/projects/ProjectsCard.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
 import styles from './projects.module.css'
-<<<<<<< HEAD
-=======
-const supportspaceImg = process.env.PUBLIC_URL + '/assets/projImg/supportspace.png';
-const taskmateImg = process.env.PUBLIC_URL + '/assets/projImg/taskmate.png';
-const marketnetImg = process.env.PUBLIC_URL + '/assets/projImg/marketnet.jpg';
-
-const imageMap = {
-    'supportspace.png': supportspaceImg,
-    'taskmate.png': taskmateImg,
-    'marketnet.jpg': marketnetImg,
-};
->>>>>>> 0b07cfbcddc4968a73e784797c0e91099a8e944e
-
 export const ProjectsCard = ({
     title, imageSrc, description, skills, githubLink, liveDemo
 }) => {
     return (
         <div className={styles.projectCard}>
-            <img src={`/projImg/${imageSrc}`} alt={title} className={styles.image} />
+            <img src={`/${imageSrc}`} alt={title} className={styles.image} />
             <div className={styles.cardContent}>
                 <h3 className={styles.title}>{title}</h3>
                 <p className={styles.description}>{description}</p>
