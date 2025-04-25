ef4f712a81747f4e4087fa90388a585f29aa383a
Your Name <you@example.com>
2025-04-25 17:17:05 +0400

    Fixes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useState , useEffect} from 'react'
+import React, {useState } from 'react'
 import styles from './navbar.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons';
