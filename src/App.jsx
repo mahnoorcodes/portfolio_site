ca809abb2613086e49acf3b7c6fa990cc6114756
Your Name <you@example.com>
2025-04-25 17:12:55 +0400

    Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import styles from './App.module.css';
-import { Navbar } from './components/navbar/navbar.jsx';
+import { Navbar } from './components/navbar/Navbar.jsx';
 import { Hero } from './components/hero/Hero.jsx';
 import { About } from './components/about/About.jsx';
 import { Skills } from './components/skills/Skills.jsx';
