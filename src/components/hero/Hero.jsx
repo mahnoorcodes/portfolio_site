7012fc4ba60c3c943ced7bf65a01e37ab41f9031
Your Name <mahnoorf.2015@gmail.com>
2025-10-14 21:20:35 +0400

    Hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaDownload, FaLinkedin, FaGithub, FaEnvelope} from 'react-icons/fa';
 import styles from './hero.module.css';
 import Spline from '@splinetool/react-spline';
 
@@ -13,7 +14,54 @@ export const Hero = () => {
             I'm Mahnoor Faisal <br />
             Web Developer | Front-End Developer | UI/UX Enthusiast | Problem Solver
           </p>
-        </div>
+          <a
+            href="https://docs.google.com/document/d/1EoF3uQ2ADoFM8KLTveH_Rdi29of-D_eGV4BRjfF_Of4/edit?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.downloadBtn}
+          >
+            Download Resume <FaDownload style={{ marginLeft: '8px' }} />
+          </a>
+          {/* Contact Icons */}
+          <div className={styles.socialIcons}>
+            <a
+              href="https://www.linkedin.com/in/mahnoor-faisal-36493a2ba/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="LinkedIn"
+            >
+              <FaLinkedin />
+            </a>
+
+            <a
+              href="https://github.com/mahnoorcodes"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="GitHub"
+            >
+              <FaGithub />
+            </a>
+
+            <a
+              href="https://www.upwork.com/freelancers/~0123c8d2af1fb979c6?mp_source=share"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Upwork"
+              className={styles.upworkText}
+            >
+              Upwork
+            </a>
+
+            <a
+              href="mailto:mahnoor.faisal.h@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Email"
+            >
+              <FaEnvelope />
+            </a>
+          </div>
+        </div>       
       </div>
 
       {/* Right Column */}
