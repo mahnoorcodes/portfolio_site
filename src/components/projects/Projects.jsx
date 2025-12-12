10c7daef3e1b3840bae7b94ae4b973fd1ef120e8
Your Name <mahnoorf.2015@gmail.com>
2025-12-12 18:05:10 +0400

    feat: update projects carousel

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from 'react';
 import styles from './projects.module.css'
 import projects from "../data/projects.json";
 import { ProjectsCard } from './ProjectsCard';
+import Slider from 'react-slick';
 
 export const Projects = () => {
   const titleRef = useRef(null);
@@ -20,14 +21,39 @@ export const Projects = () => {
   
           observer.observe(el);
       }, []);
+      
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    
+
+        responsive: [
+      {
+        breakpoint: 480, 
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <section className={styles.projectSection} id="projects">
       <h1 ref={titleRef} className={styles.title}>PROJECTS</h1>
-      <div className={styles.projects}>
-      {projects.map((projects, id) => {
-        return <ProjectsCard key={id} {...projects} />;
-      })}
-      </div>
+      <Slider {...sliderSettings}>
+        {projects.map((project, id) => {
+          return (
+            <div key={id} className={styles.projectSlide}>
+              <ProjectsCard {...project} />
+            </div>
+          );
+        })}
+      </Slider>
     </section>
   );
 };
